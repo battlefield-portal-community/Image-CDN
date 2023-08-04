9a43c3429c2818f2fd0c72114b3aa058a2b9c792
p0lygun <solankivibhakar82@gmail.com>
2023-08-05 01:48:07 +0530

    chore: update deps

--- scripts/portal_blocks_extractor/script.py
+++ scripts/portal_blocks_extractor/script.py
@@ -9,7 +9,6 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
